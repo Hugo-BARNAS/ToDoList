a78843b967d1e4b09c9208d8120c0879090a7860
Hugo Barnas <hugo@MacBook-Pro-de-Hugo.local>
2020-07-05 18:51:30 +0200

    Ajout du fichier js dans le html

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
 // CODE EXPLAINED channel
+
+// SELECTION DES ELEMENTS
+
+const clear = document.querySelector(".clear");
+const dataElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+
+// CREATION DES CLASSES
+
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-fin";
+const LINE_THROUGH = "lineThrought";
+
+// DATE DU JOUR
+const options = { weekday: "long", month: "short", day: "2-digit"};
+const today = new Date();
+let calendar = dataElement.innerHTML = today.toLocaleDateString('fr-FR', options);
+
+
+console.log("DATE ==> : " + calendar);
+
